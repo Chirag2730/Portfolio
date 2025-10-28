ad6f61f43f888dfdd540dd020f9ce6fc35c8aa3b
Chirag2730 <00chiragsingh00@gmail.com>
2025-10-28 20:28:03 +0530

    First commit

---
+++ src/constants/index.js
@@ -0,0 +1,337 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "Unify",
+    description:
+      "A Real-Time Video Calling & Chat App made using MERN, TanStack Query, JWT, Tailwind.",
+    subDescription: [
+      "Built secure authentication system using JWT with protected routes, custom hooks and rigorous API testing, ensuring safe and reliable user access.",
+      "Developed real-time chat and video calling features, enabling instant communication and enhancing user experience.",
+      "Created 32 customizable UI themes with Tailwind, delivering a personalized interface for diverse user preferences.",
+      "Designed and implemented an onboarding flow with friend management, improving user engagement and retention.",
+    ],
+    href: "https://unify-rplr.onrender.com",
+    logo: "",
+    image: "/assets/projects/unify.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 5,
+        name: "GetStream",
+        path: "/assets/logos/stream.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Cashtrack",
+    description:
+      "An Expense Tracking Application made with MERN, Tailwind, Chart.js .",
+    subDescription: [
+      "Designed an interactive dashboard showcasing balance, income, and expenses for clear financial insights along with functionality to add, view, and delete income/expenses with category tracking, enabling structured money management.",
+      "Implemented JWT-based authentication, securing sensitive financial data for all users.",
+      "Integrated Excel export feature for reports, providing flexibility for offline usage and record-keeping.",
+      "Visualized financial data with interactive bar, pie, and line charts, making spending patterns easy to analyze.",
+    ],
+    href: "https://cashtrack-1ipl.onrender.com",
+    logo: "",
+    image: "/assets/projects/cashtrack.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongodb.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 5,
+        name: "Chart.js",
+        path: "/assets/logos/chartjs.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "3D Portfolio Website",
+    description:
+      "A modern, interactive developer portfolio using 3D visuals to showcase technical and creative skills.",
+    subDescription: [
+      "Designed and developed a fully interactive 3D developer portfolio using Three.js and React, showcasing key projects and technical skills in a visually engaging format.",
+      "Ensured cross-browser compatibility and mobile responsiveness through adaptive layout techniques and media queries.",
+      "Applied performance optimization strategies (e.g., lazy loading models, minimizing draw calls) to ensure smooth performance on low-end devices.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/portfolio.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "Motion",
+        path: "/assets/logos/motion.svg",
+      },
+      {
+        id: 3,
+        name: "Three.js",
+        path: "/assets/logos/threejs.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "AI-Powered Resume Analyzer",
+    description:
+      "An AI-powered Resume Analyzer with React, React Router, and Puter.js!.",
+    subDescription: [
+      "Built a powerful rendering engine using OpenGL and C++.",
+      "Developed a physics engine with collision detection and particle effects.",
+      "Implemented a scripting system for easy game customization.",
+      "Optimized performance with multi-threading and efficient memory management.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/game-engine.jpg",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "TypeScript",
+        path: "/assets/logos/typescript.svg",
+      },
+      {
+        id: 3,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+      {
+        id: 4,
+        name: "Puter",
+        path: "/assets/logos/puter.svg",
+      },
+    ],
+  },
+//   {
+//     id: 5,
+//     title: "WordPress Custom Theme",
+//     description:
+//       "A fully customizable WordPress theme optimized for performance and SEO.",
+//     subDescription: [
+//       "Developed a responsive WordPress theme using HTML5, CSS3, and JavaScript.",
+//       "Integrated Tailwind CSS for modern styling and UI enhancements.",
+//       "Optimized SEO and page speed using Vite.js for fast builds.",
+//       "Implemented custom widgets and plugin compatibility for extended functionality.",
+//     ],
+//     href: "",
+//     logo: "",
+//     image: "/assets/projects/wordpress-theme.jpg",
+//     tags: [
+//       {
+//         id: 1,
+//         name: "WordPress",
+//         path: "/assets/logos/wordpress.svg",
+//       },
+//       {
+//         id: 2,
+//         name: "HTML5",
+//         path: "/assets/logos/html5.svg",
+//       },
+//       {
+//         id: 3,
+//         name: "CSS3",
+//         path: "/assets/logos/css3.svg",
+//       },
+//       {
+//         id: 4,
+//         name: "Vite.js",
+//         path: "/assets/logos/vitejs.svg",
+//       },
+//     ],
+//   },
+//   {
+//     id: 6,
+//     title: "Online Learning Platform",
+//     description:
+//       "A web application that allows users to enroll in courses, watch video lectures, and take quizzes.",
+//     subDescription: [
+//       "Built using Blazor WebAssembly for a seamless SPA experience.",
+//       "Implemented video streaming with Azure Media Services.",
+//       "Added a quiz system with dynamic question generation and real-time grading.",
+//       "Integrated Stripe API for secure payment processing.",
+//     ],
+//     href: "",
+//     logo: "",
+//     image: "/assets/projects/elearning.jpg",
+//     tags: [
+//       {
+//         id: 1,
+//         name: "Blazor",
+//         path: "/assets/logos/blazor.svg",
+//       },
+//       {
+//         id: 2,
+//         name: "Azure",
+//         path: "/assets/logos/azure.svg",
+//       },
+//       {
+//         id: 3,
+//         name: "Stripe",
+//         path: "/assets/logos/stripe.svg",
+//       },
+//       {
+//         id: 4,
+//         name: "TailwindCSS",
+//         path: "/assets/logos/tailwindcss.svg",
+//       },
+//     ],
+//   },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/ali-sanati/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/ali.sanatidev/reels/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Software Developer",
+    job: "Security & Defense Projects",
+    date: "2021-2023",
+    contents: [
+      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
+      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
+      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
+      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
+      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+    ],
+  },
+  {
+    title: "Back-End Developer",
+    job: "Car Manufacture",
+    date: "2023-2024",
+    contents: [
+      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
+      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
+      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
+      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
+      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+    ],
+  },
+  {
+    title: "Freelance Developer",
+    job: "Self-Employed",
+    date: "2025-Present",
+    contents: [
+      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
+      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+    ],
+  },
+];
+export const reviews = [
+  {
+    name: "Jack",
+    username: "@jack",
+    body: "I've never seen anything like this before. It's amazing. I love it.",
+    img: "https://robohash.org/jack",
+  },
+  {
+    name: "Jill",
+    username: "@jill",
+    body: "I don't know what to say. I'm speechless. This is amazing.",
+    img: "https://robohash.org/jill",
+  },
+  {
+    name: "John",
+    username: "@john",
+    body: "I'm at a loss for words. This is amazing. I love it.",
+    img: "https://robohash.org/john",
+  },
+  {
+    name: "Alice",
+    username: "@alice",
+    body: "This is hands down the best thing I've experienced. Highly recommend!",
+    img: "https://robohash.org/alice",
+  },
+  {
+    name: "Bob",
+    username: "@bob",
+    body: "Incredible work! The attention to detail is phenomenal.",
+    img: "https://robohash.org/bob",
+  },
+  {
+    name: "Charlie",
+    username: "@charlie",
+    body: "This exceeded all my expectations. Absolutely stunning!",
+    img: "https://robohash.org/charlie",
+  },
+  {
+    name: "Dave",
+    username: "@dave",
+    body: "Simply breathtaking. The best decision I've made in a while.",
+    img: "https://robohash.org/dave",
+  },
+  {
+    name: "Eve",
+    username: "@eve",
+    body: "So glad I found this. It has changed the game for me.",
+    img: "https://robohash.org/eve",
+  },
+];
